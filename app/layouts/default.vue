ecefd4a763649d018c75e8b15ed1ea6a0fe5ea34
LarsvandenBosch <lars@skeps.nl>
2025-10-03 17:49:05 +0200

    chore: added layout file for reusable components

---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<Navbar />
+		<div class="pt-28">
+			<Sidebar />
+			<main>
+				<slot />
+			</main>
+		</div>
+	</div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,22 +1,17 @@
 <template>
 	<div>
-		<Navbar />
-
-		<div class="pt-28">
-			<Sidebar />
-			<div class="flex flex-col gap-4 lg:ml-96 px-4 py-6 lg:px-16 lg:py-12 min-h-screen text-[#0C0D0E]">
-				<textarea
-					placeholder="De planning voor morgen..."
-					class="bg-transparent border-none outline-none font-semibold text-[2.5rem] tracking-tight"
-					maxlength="30"
-					rows="1"
-				/>
-				<textarea
-					placeholder="Morgen ga ik naar school en daarna..."
-					class="bg-transparent border-none outline-none font-normal leading-6"
-					rows="30"
-				/>
-			</div>
+		<div class="flex flex-col gap-4 lg:ml-96 px-4 py-6 lg:px-16 lg:py-12 min-h-screen text-[#0C0D0E]">
+			<textarea
+				placeholder="De planning voor morgen..."
+				class="bg-transparent border-none outline-none font-semibold text-[2.5rem] tracking-tight"
+				maxlength="30"
+				rows="1"
+			/>
+			<textarea
+				placeholder="Morgen ga ik naar school en daarna..."
+				class="bg-transparent border-none outline-none font-normal leading-6"
+				rows="30"
+			/>
 		</div>
 	</div>
 </template>
