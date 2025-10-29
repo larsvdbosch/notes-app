1b31d89d60d0d06e4928a587dbed31a12e3a3354
LarsvandenBosch <lars@skeps.nl>
2025-10-29 19:48:38 +0100

    feat: added weird hero background pattern

--- app/components/Hero.vue
+++ app/components/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<div class="border-b border-[var(--color-border)] bg-[image:repeating-linear-gradient(315deg,_var(--color-pattern)_0,_var(--color-pattern)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed">
 		<section class="container padding-section flex justify-center px-4">
 			<div class="flex items-center text-[var(--color-text)] gap-4 flex-col">
 				<h1 class="heading-hero max-w-3xl">
--- app/layouts/dashboard.vue
+++ app/layouts/dashboard.vue
@@ -4,7 +4,7 @@
 		<DashboardNavbar />
 		<div class="pt-40">
 			<DashboardSidebar />
-			<main class="bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed">
+			<main class="bg-[image:repeating-linear-gradient(315deg,_var(--color-pattern)_0,_var(--color-pattern)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed">
 				<slot />
 			</main>
 		</div>
